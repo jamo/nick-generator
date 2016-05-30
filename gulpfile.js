88ee654e06410af59f730d563f27811d25cf6497
Jarmo Isotalo <jamo@isotalo.fi>
2016-05-30 17:41:18 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const cache = require('gulp-cached')
+const chmod = require('gulp-chmod')
+
+const paths = {
+  index: 'index.js'
+}
+
+gulp.task('index', () => {
+  return gulp.src(paths.index)
+  .pipe(cache('index'))
+  .pipe(babel())
+  .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch(paths.index, ['index'])
+})
+
+gulp.task('transpile', ['index'])
+gulp.task('default', ['watch', 'transpile'])
+
